95b4d8ee4b0579695ce9047035deb7a98b89e29c
Rudra Choudhary <rudrachoudhary067@gmail.com>
2026-04-07 16:47:09 +0530

    deploy phase 1

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,8 @@ const ConnectToDb = require('./src/config/database')
 
 ConnectToDb();
 
-app.listen(3000, () => {
-    console.log("Server Is Running On Port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });

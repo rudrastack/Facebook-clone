da8ccfeda5fb0ebc8cbc31dd43c55e6c5213d5ac
Rudra Choudhary <rudrachoudhary067@gmail.com>
2026-04-07 17:27:14 +0530

    connect frontend to deployed backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "https://your-frontend-url.onrender.com" }))
+app.use(cors({ credentials: true, origin: "https://facebook-clone-3vpt.onrender.com" }))
 app.use('/api/authfb', authRoutes);   
 app.use('/api/followfb', followRoutes);   
 app.use('/api/postfb',postRoutes );   

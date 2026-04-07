a9cc7f7a9a7fce417ea10d1e4f900d6c6449e1a2
Rudra Choudhary <rudrachoudhary067@gmail.com>
2026-04-07 17:17:26 +0530

    fix: correct import path case issue

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -11,7 +11,7 @@ export const AuthProvider = ({ children }) => {
         const storedUser = localStorage.getItem("user")
 
         if (storedUser) {
-            setUser(JSON.parse(storedUser)) // ✅ ONLY THIS
+            setUser(JSON.parse(storedUser))
         }
 
         setLoading(false)
--- Frontend/src/features/posts/pages/Feed.jsx
+++ Frontend/src/features/posts/pages/Feed.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import "../style/feed.scss"
-import Post from '../components/Post'
+import Post from '../components/post'
 import { usePost } from '../hooks/usePost'
 import Nav from '../../shared/components/Nav'
 import { useAuth } from '../../auth/hooks/useAuth' 

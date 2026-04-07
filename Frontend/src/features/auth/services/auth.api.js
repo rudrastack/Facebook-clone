e8368d28f7d25baf0c51c3e055cf6877911a5e7f
Rudra Choudhary <rudrachoudhary067@gmail.com>
2026-04-07 17:26:17 +0530

    connect frontend to deployed backend

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/authfb",
+    baseURL: "https://facebook-clone-3vpt.onrender.com/api/authfb",
     withCredentials: true,
 })
 
